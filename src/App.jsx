322839d684a407325c464d3ce45468df8f0385ec
William <willspecker@gmail.com>
2020-11-24 21:51:31 +0100

    adding basic framework of handleUserChoice function with prototype for score state, add function to randomize computer choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
 import React, { Component } from 'react'
 import RpsButtons from './components/RpsButtons';
 import { Button , Icon } from 'semantic-ui-react'
-import { rpsGame} from './helpers/rpsHelper';
+import { rpsGame } from './helpers/rpsHelper';
 import  Selector from './components/Selector'
 
  class App extends Component {
-   
+   state = {
+     score: 0
+   }
 
+  
+
+  handleUserChoice(props) {
+    alert(`You chose ${props}`);
+    this.setState({ score: this.state.score + 1 })
+  };
+
+  getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice
+  };
+
+  
   render() {
     return (
       <div>
-          <button id='rock'>
+          <button id='rock' name='rock' onClick={() => this.handleUserChoice('rock')}>
             Rock
           </button>
-          <button id='paper'>
+          <button id='paper' name='paper' onClick={() => this.handleUserChoice('paper')}>
             Paper
           </button>
-          <button id='scissors'>
+          <button id='scissors' name='scissors' onClick={() => this.handleUserChoice('scissors')}>
             Scissors
           </button>
+          <p>
+            Your Score: {this.state.score}
+          </p>
       </div>
     )
   }
