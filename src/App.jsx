f0c423c7249b0ada672d2a0d1629bba7715c2175
William <willspecker@gmail.com>
2020-11-26 20:51:36 +0100

    formatting code in render function

--- src/App.jsx
+++ src/App.jsx
@@ -57,39 +57,79 @@ import './styling/App.css';
     return (
       <body id='body'>
       <Container id='container'>
-        <Header as='h1' id='header'>Welcome To Rock/Paper/Scissors!</Header>
-        <Header as='h2' id='header-2'>Click To Start Playing</Header>
-      <Grid id='rps-grid' columns={3}>
-        <Grid.Row>
-        <Grid.Column>
-          <Button color='red' size='massive' animated='fade' id='rock' name='rock' onClick={() => this.handleUserChoice('rock')}>
-            <Button.Content visible><Icon size='large' color='white' name='hand rock'></Icon></Button.Content>
-            <Button.Content hidden>Rock</Button.Content>
-          </Button>
-        </Grid.Column>
-        <Grid.Column>
-          <Button size='massive' color='green' animated='fade' id='paper' name='paper' onClick={() => this.handleUserChoice('paper')}>
-            <Button.Content visible><Icon size='large' color='white' name='hand paper'></Icon></Button.Content>
-            <Button.Content hidden>Paper</Button.Content>
-          </Button>
-        </Grid.Column>
-        <Grid.Column>
-          <Button size='massive' color='purple' animated='fade' id='scissors' name='scissors' onClick={() => this.handleUserChoice('scissors')}>
-            <Button.Content visible><Icon size='large' color='white' name='hand scissors'></Icon></Button.Content>
-            <Button.Content hidden>Scissors</Button.Content>
-          </Button>
-        </Grid.Column>
-        </Grid.Row>
-      </Grid>
+        <Header as='h1' id='header'>
+          Welcome To Rock/Paper/Scissors!
+        </Header>
+        <Header as='h2' id='header-2'>
+          Click To Start Playing
+        </Header>
+        <Grid id='rps-grid' columns={3}>
+          <Grid.Row>
+            <Grid.Column>
+              <Button 
+                size='massive' 
+                color='red' 
+                animated='fade' 
+                id='rock' 
+                name='rock' 
+                onClick={() => this.handleUserChoice('rock')}
+              >
+                <Button.Content visible>
+                  <Icon size='large' color='white' name='hand rock'></Icon>
+                </Button.Content>
+                <Button.Content hidden>
+                  Rock
+                </Button.Content>
+              </Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button 
+                size='massive' 
+                color='green' 
+                animated='fade' 
+                id='paper' 
+                name='paper' 
+                onClick={() => this.handleUserChoice('paper')}
+              >
+                <Button.Content visible>
+                  <Icon size='large' color='white' name='hand paper'></Icon>
+                </Button.Content>
+                <Button.Content hidden>
+                  Paper
+                </Button.Content>
+              </Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button 
+                size='massive' 
+                color='purple' 
+                animated='fade' 
+                id='scissors' 
+                name='scissors' 
+                onClick={() => this.handleUserChoice('scissors')}
+              >
+                <Button.Content visible>
+                  <Icon size='large' color='white' name='hand scissors'></Icon>
+                </Button.Content>
+                <Button.Content hidden>
+                  Scissors
+                </Button.Content>
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
           <Message id='message' icon>
             <Icon color='red' name='star' />
             <Message.Content>
-            <Message.Header>Scoreboard
-            </Message.Header>
-            <Message.List>
-            <Message.Item>You: {this.state.score.user}</Message.Item>
-            <Message.Item>Computer: {this.state.score.computer}</Message.Item>
-            </Message.List>
+              <Message.Header>Scoreboard</Message.Header>
+              <Message.List>
+                <Message.Item>
+                  You: {this.state.score.user}
+                </Message.Item>
+                <Message.Item>
+                  Computer: {this.state.score.computer}
+                </Message.Item>
+              </Message.List>
             </Message.Content>
           </Message>
       </Container>
--- src/components/RpsButtons.jsx
+++ src/components/RpsButtons.jsx
@@ -7,24 +7,7 @@ const RpsButtons = () => {
 
   return (
     <div>
-    <Button animated='fade'>
-      <Button.Content visible>
-        <Icon name='hand rock'/>
-      </Button.Content>
-      <Button.Content hidden>Rock</Button.Content>
-    </Button>
-    <Button animated='fade'>
-      <Button.Content visible>
-        <Icon name='hand scissors'/>
-      </Button.Content>
-      <Button.Content hidden>Scissors</Button.Content>
-    </Button>
-    <Button animated='fade'>
-      <Button.Content visible>
-        <Icon name='hand paper'/>
-      </Button.Content>
-      <Button.Content hidden>Paper</Button.Content>
-    </Button>
+    
   </div>
   )
 }
