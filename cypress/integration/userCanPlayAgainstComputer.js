c8b55bcc1d016c8bf606a84adc18bda125bfcfe0
William <willspecker@gmail.com>
2020-11-27 11:13:18 +0100

    updating test to check for button content when clicking button, stub still WIP

--- cypress/integration/userCanPlayAgainstComputer.js
+++ cypress/integration/userCanPlayAgainstComputer.js
@@ -1,24 +1,24 @@
 describe('RPS Game', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000'), {
-      onBeforeLoad(window) {
-        cy.stub(window.Math, "random").returns(1)
+      onBeforeLoad(win) {
+        cy.stub(win, 'getComputerChoice').returns('rock')
       }
     }
   })
 
   it('Plays Rock', () => {
-    cy.get('button#rock').click();
-    cy.get('#message').should('contain', 'You: 1')
+    cy.get('button#rock').contains('Rock').click();
+    cy.get('#message').should('contain', 'You: 0')
   })
 
   it('Plays Scissors', () => {
-    cy.get('button#scissors').click();
-    cy.get('#message').should('contain', 'Computer')
+    cy.get('button#scissors').contains('Scissors').click();
+    cy.get('#message').should('contain', 'Computer: 1')
   })
 
   it('Plays Paper', () => {
-    cy.get('button#paper').click();
-    cy.get('#message').should('contain', 'You')
+    cy.get('button#paper').contains('Paper').click();
+    cy.get('#message').should('contain', 'You: 1')
   })
 })
